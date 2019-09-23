bf8815983bd22a6b3ae875439d6792998f6b139a
fjaviersaezm <fjaviersaezm@gmail.com>
2019-09-23 02:41:44 +0200

    Working on exercise3, fails if images dont have same size

--- Práctica 0/exercises.py
+++ Práctica 0/exercises.py
@@ -3,30 +3,34 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-def normalized(a, axis=-1, order=2):
-    l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
-    l2[l2==0] = 1
-    return a / np.expand_dims(l2, axis)
 
 def leeimagen(filename,flagColor):
     image = cv2.imread(filename,flagColor)
 
     #Converts BRG to RGB, important
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.show()
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
 
     return image
 
 def pintaI(im):
 
-    plt.imshow(im)
-    plt.show()
-    plt.waitKey(0)
+    cv2.imshow('matrix',im)
+    cv2.waitKey(0)
 
 def pintaMI(vim):
 
-    return 0
+    #Added because if it 
+    #h = max(image.shape[0] for image in vim)
 
+    for i,im in enumerate(vim):
+        #im.shape contains rows, columns and channel (if image is color). if len = 2, it means there is no color.
+        if len(im.shape)== 2:
+            vim[i] = cv2.cvtColor(vim[i],cv2.COLOR_GRAY2BGR)
+
+    #hconcat creates a new imagen concatenating a list of images
+    images = cv2.hconcat(vim)
+    pintaI(images)
 
 
 #Flags can be used like:
@@ -35,12 +39,28 @@ def pintaMI(vim):
 
 #Ejercicio 1
 img = leeimagen("images/orapple.jpg",0)
-img = leeimagen("images/orapple.jpg",1)
+img2 = leeimagen("images/orapple.jpg",1)
 
+cv2.destroyAllWindows()
 
 #Ejercicio 2
 matrix = np.random.random((100, 100))*956
-print(matrix)
-matrix = matrix / np.linalg.norm(matrix)
-print(matrix)
 pintaI(matrix)
+#Normalize
+matrix = matrix / np.linalg.norm(matrix)
+pintaI(matrix)
+
+cv2.destroyAllWindows()
+
+#Ejercicio 3
+
+#Si no transformamos las imágenes, obtenemos un error en la función hconcat de esta llamada. El error es:
+#src[i].dims <= 2 && src[i].rows == src[0].rows && src[i].type() == src[0].type() in function 'hconcat'
+
+pintaMI([img,img2])
+
+cv2.destroyAllWindows()
+
+img3 = leeimagen("images/logoOpenCV.jpg",0)
+
+pintaMI([img,img3])
